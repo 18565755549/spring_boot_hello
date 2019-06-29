a4789930590c1cf9d32ac2dd04ad59b40e020265
admin <admin@qq.com>
2019-06-29 12:21:22 +0800

    hello boot

---
+++ src/main/java/xin/zhangzijun/HelloBoot.java
@@ -0,0 +1,13 @@
+package xin.zhangzijun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloBoot {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HelloBoot.class,args);
+    }
+}
